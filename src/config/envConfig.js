0a3c714e3adcc3400d9ec2c47fc1066a9ba2dc0b
CodeMaestroAyodeji <ajuwon2024@gmail.com>
2024-12-17 02:52:22 +0100

    Updating the package.json file

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -2,7 +2,7 @@
 
 const apiUrl =  
   import.meta.env.VITE_NODE_ENV === 'production'
-    ? 'https://torrent-caching-backend.vercel.app/' // Production URL  
+    ? 'https://torrent-caching-backend.vercel.app' // Production URL  
     : import.meta.env.VITE_API_URL || 'http://localhost:5000'; // Development URL  
 
 export default apiUrl;
