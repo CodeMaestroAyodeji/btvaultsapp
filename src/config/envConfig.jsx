790ea2712ba7a454af44b1f3c68fe6df9d716444
CodeMaestroAyodeji <ajuwon2024@gmail.com>
2024-12-25 01:28:55 +0530

    updating the .env

--- src/config/envConfig.jsx
+++ src/config/envConfig.jsx
@@ -1,7 +1,7 @@
 // src/config/envConfig.jsx
 
 const apiUrl = import.meta.env.MODE === 'production' 
-  ? import.meta.env.VITE_API_URL 
-  : import.meta.env.VITE_PROD_API_URL;
+  ? import.meta.env.VITE_PROD_API_URL 
+  : import.meta.env.VITE_API_URL;
 
 export default apiUrl;
