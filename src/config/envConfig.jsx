331297cd7e545309b6e4f1646de98d838df64660
CodeMaestroAyodeji <ajuwon2024@gmail.com>
2024-12-25 08:56:01 +0530

    updating the encConfig.jsx

--- src/config/envConfig.jsx
+++ src/config/envConfig.jsx
@@ -1,7 +1,11 @@
 // src/config/envConfig.jsx
+ 
+const apiUrl = import.meta.env.MODE === 'production'   
+  ? import.meta.env.VITE_PROD_API_URL   
+  : import.meta.env.VITE_API_URL;  
 
-const apiUrl = import.meta.env.MODE === 'production' 
-  ? import.meta.env.VITE_PROD_API_URL 
-  : import.meta.env.VITE_API_URL;
+export default apiUrl;  
 
-export default apiUrl;
+// Make sure you have .env variables set up correctly as below:  
+// VITE_PROD_API_URL=https://torrent-caching-backend.vercel.app  
+// VITE_API_URL=http://localhost:5000
